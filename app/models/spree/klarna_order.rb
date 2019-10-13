54dcbddb5f0b6e740cba961f06d5293579681217
lordvladhack <vlad.dicusara@outlook.com>
2019-10-13 07:13:16 +0300

    include KlarnaOrder into Spree module namespace

--- app/models/spree/klarna_order.rb
+++ app/models/spree/klarna_order.rb
@@ -1,3 +1,5 @@
-class KlarnaOrder < Spree::Base
-    belongs_to: spree_order, class_name: 'Spree::Order', foreign_key: 'spree_order_id'
+module Spree
+    class KlarnaOrder < Spree::Base
+        belongs_to: spree_order, class_name: 'Spree::Order', foreign_key: 'spree_order_id'
+    end
 end
