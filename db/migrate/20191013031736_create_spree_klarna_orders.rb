f1020165b42853441dcb05a19d2dd8343f7b0afa
lordvladhack <vlad.dicusara@outlook.com>
2019-10-13 08:01:58 +0300

    rename klarna_order_id to order_id

--- db/migrate/20191013031736_create_spree_klarna_orders.rb
+++ db/migrate/20191013031736_create_spree_klarna_orders.rb
@@ -3,7 +3,7 @@ class CreateSpreeKlarnaOrders < ActiveRecord::Migration
     create_table :spree_klarna_orders do |t|
       t.belongs_to :spree_order, index: { unique: true }, foreign_key: true
       t.integer :spree_order_id
-      t.string :klarna_order_id
+      t.string :order_id
       t.string :klarna_order_state
       t.text :klarna_session_id
       t.text :klarna_client_token
