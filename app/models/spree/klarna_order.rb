68869a840a9c0a4a2325b741d863492ea0b7068a
lordvladhack <vlad.dicusara@outlook.com>
2019-10-13 07:56:32 +0300

    fix clarnaorder syntax

--- app/models/spree/klarna_order.rb
+++ app/models/spree/klarna_order.rb
@@ -1,5 +1,5 @@
 module Spree
     class KlarnaOrder < Spree::Base
-        belongs_to: spree_order, class_name: 'Spree::Order', foreign_key: 'spree_order_id'
+        belongs_to :spree_order, class_name: 'Spree::Order'
     end
 end
